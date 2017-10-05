a6dc2dd30ea2322a33a34cadf402a95fac1385dd
m081779 <marcoprincipio@hotmail.com>
2017-10-05 15:06:29 -0400

    many changes, added carousel, changed button structure, added events to remove buttons etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,92 @@ $(document).ready(function () {
 
 //giphy application object
 var giphy = {
-		//initializing array to store buttons
+		//initializing variables
 		rating: '',
+		response: '',
 		buttonArr: [],	
 
 		//method that creates and appends buttons
 		createButton: function () {
 			//clears any previous buttons so buttons aren't re-added
 			$('.button-box').empty();
+			//writes headline to .button-box once buttons are created
+			$('.button-box').html('<h4>Click a button below to display images</h4>');
 			//loop that creates buttons for every index of buttonArr
 			for (var i = 0; i<giphy.buttonArr.length; i++) {
-				//creates a button element in memory with bootstrap classes
-				var btn = $('<button class="button btn btn-primary btn-large">');
-				//sets text of the button to the string at index i and appends btn to .button-box
-				btn.text(giphy.buttonArr[i])
-				   .appendTo('.button-box');
+				//creates div to hold .removeButton and .itemButton
+				var divButton = $('<div class="divButton">')
+				//creates a div in memory with itemButton class
+				var itemButton = $('<div class="itemButton"></div>');
+				//writes the search text to the div
+				itemButton.text(giphy.buttonArr[i]);
+				//creates a div in memory with removeButton class
+				var removeButton = $('<div class="removeButton">')
+				//gives it text of 'X'
+				removeButton.text('X');
+				//appends removeButton and itemButton to divButton
+				divButton.append(removeButton,itemButton)
+				//appends divButtons to .button-box
+				divButton.appendTo('.button-box');
 			}//end of for loop
 		},//end of createButton method
 
+		//method that dynamically creates carousel structure
+		showCarousel: function () {
+			$('.carousel-box').empty();
+			//dynamically creates the carousel structure so that the generateCarouselItems method can populate it
+			var carouselElement = '<div id="myCarousel" class="carousel slide spaceBelow" data-ride="carousel">'+
+								'<ol class="carousel-indicators"></ol>'+
+								'<div class="carousel-inner" role="listbox"></div>'+
+								'<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">'+
+								'<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>'+
+								'<span class="sr-only">Previous</span></a>'+
+								'<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">'+
+								'<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'+
+								'<span class="sr-only">Next</span></a></div>';
+			//inserts the carouselElement in the correct spot in the DOM
+			$(carouselElement).appendTo('.carousel-box');
+		},
+
+		//method that loops through response and generates carousel items
+		generateCarouselItems: function () {
+			//loops through all of the gifs returned in the request
+    		for (var i = 0; i<giphy.response.data.length; i++) {
+    			//creates an image element in memory
+				var img = $("<img class='img img-thumbnail'>");
+				//creates .item div's to store images for carousel
+				var div = $('<div class="item">');
+				//creates li's for the indicator circles on carousel
+				var li = $('<li>');
+				//sets data-target and data-slide-to for carousel
+				li.attr({
+					'data-target': '#myCarousel',
+					'data-slide-to': i
+				});
+				//appends new li's to .carousel-indicators
+				$(li).appendTo('.carousel-indicators');
+				//sets attributes that store urls for still and animated gifs, and the current state
+				img.attr({
+					'src':  giphy.response.data[i].images.fixed_height_still.url,
+					'data-animate': giphy.response.data[i].images.fixed_height.url,
+					'data-still': giphy.response.data[i].images.fixed_height_still.url, 
+					'data-state':'still',
+					'alt': 'giphy' + i
+					
+				//and appends them the .item container
+				}).appendTo(div);
+				//appends .item divs to .carousel-inner
+				$(div).appendTo('.carousel-inner');
+				//adds active class to first .item and li to initialize carousel
+				$('.item').first().addClass('active');
+				$('li').first().addClass('active');
+			}//end of for loop
+		},
+
 		//method that executes an ajax request and displays results to screen
 		showGiphy: function () {
-
 			//empties .display of previous gifs
-			$('.display').empty();
+			$('.display').empty();	
 			//code that structures the ajax query url
 			giphy.queryURL = "https://api.giphy.com/v1/gifs/search";
 			giphy.queryURL += '?' + $.param({
@@ -33,25 +96,16 @@ var giphy = {
 				'limit': "10",
 				'rating': giphy.rating
 			});
-			console.log(giphy.queryURL);
 			//ajax request
 			$.ajax({
 				url: giphy.queryURL,
 				method: 'GET'
 		    }).done(function(obj) {
-		    	//loops through all of the gifs returned in the request
-	    		for (var i = 0; i<obj.data.length; i++) {
-	    			//creates an image element in memory
-					var img = $("<img class='img'>");
-					//sets attributes that store urls for still and animated gifs, and the current state
-					img.attr({
-						'src':  obj.data[i].images.fixed_height_still.url,
-						'data-animate': obj.data[i].images.fixed_height.url,
-						'data-still': obj.data[i].images.fixed_height_still.url, 
-						'data-state':'still'
-					//and appends them to the element with class 'display'	
-					}).appendTo('.display');
-				}//end of for loop
+		    	//stores reference to response object as a variable
+		    	giphy.response = obj;
+		    	//runs methods that generate and show the carousel of response data
+		    	giphy.showCarousel();
+		    	giphy.generateCarouselItems();
 		    });//end of done method
 		}//end of showGiphy method
 
@@ -59,10 +113,15 @@ var giphy = {
 
 ////////////////////////////event listeners////////////////////////////
 
+
+	//click event for rating buttons
 	$('.rating').on('click', function () {
+		//sets rating to the id of clicked button
 		giphy.rating = $(this).attr('id');
+		//conditional checks if 'any' button is clicked...
 		if (giphy.rating==='any') {
-			giphy.rating = ' '
+			//...and sets it to blank to omit rating from the search
+			giphy.rating = ' ';
 		}
 	});
 
@@ -82,24 +141,28 @@ var giphy = {
 		}
 	});
 
-	//click event that executes showGiphy when queryTerm button is clicked
-	$(document).on('click','.button',function () {
+	//delegated click event that executes showGiphy() when .itemButton is clicked
+	$(document).on('click','.itemButton',function () {
+		//conditional checks if a rating has been assigned for query...
 		if (!giphy.rating){
-			$('#rating-text').addClass('highlight');
+			//...if not checkMe class is added to remind user
+			$('#rating-text').addClass('checkMe');
 		}
+
 		else {
-			$('#rating-text').removeClass('highlight');
+			//otherwise the checkMe class is removed
+			$('#rating-text').removeClass('checkMe');
 			//sets current queryTerm to the text of clicked button
 			giphy.queryTerm = $(this).text();
 			//executes showGiphy method
 			giphy.showGiphy();
 		}
+		//sets the rating to blank to test every time a button is clicked
 		giphy.rating = '';
 	});
 
 	//keyup event that creates button on enter
 	$('#queryTerm').on('keyup', function (event) {
-		
 		//conditional that checks if enter button is pressed and input has value
 		if (event.keyCode===13 && $('#queryTerm').val()!=='') {
 			//if enter is pressed...
@@ -121,12 +184,32 @@ var giphy = {
 		if ($(this).attr('data-state')==='still') {
 			$(this).attr('src', $(this).attr('data-animate'));
 			$(this).attr('data-state', 'animate');
-		} else {
+		} 
+		else {
 			$(this).attr('src', $(this).attr('data-still'));
 			$(this).attr('data-state', 'still');
 		}
 	});
-	//creates button for any queryTerms that are stored in buttonArr on load
-	giphy.createButton();
+	//delegated click event that removes buttons when .removeButton is clicked
+	$(document).on('click', '.removeButton', function () {
+		//stores the value of itemButton's text as a variable
+		var value = $(this).siblings().text();
+		//removes the entire parent element
+		$(this).parent().remove();
+		//loop to find removed element in buttonArr
+		for (var i = 0; i<giphy.buttonArr.length; i++) {
+			//conditional checks if value to be removed is found...
+			if (giphy.buttonArr[i]===value) {
+				//...if so element is spliced out of array
+				giphy.buttonArr.splice(i, 1);
+			}
+		}
+		//conditional checks if buttonArr is empty...
+		if (giphy.buttonArr.length===0) {
+				//...if so it removes headline text
+				$('.button-box').empty();
+		}
+	});
+
 });//end of document ready function
 
